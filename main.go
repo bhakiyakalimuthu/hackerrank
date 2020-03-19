cd7fde56396b6f8d0a4504dbc3e54eb3533e5088
Bhakiyaraj Kalimuthu <bhakiya.kalimuthu@gmail.com>
2020-03-19 22:09:40 +0100

    Add model

--- main.go
+++ main.go
@@ -79,24 +79,7 @@ func client(pageNumber int) (*PageInfo, error) {
 	return &page, nil
 }
 
-type PageInfo struct {
-	Page       string     `json:"page"`
-	PerPage    int32      `json:"per_page"`
-	Total      int32      `json:"total"`
-	TotalPages int        `json:"total_pages"`
-	Data       []UserInfo `json:"data"`
-}
 
-type UserInfo struct {
-	ID              int       `json:"id"`
-	UserName        string    `json:"username"`
-	About           string    `json:"about"`
-	Submitted       int32     `json:"submitted"`
-	UpdatedAt       time.Time `json:"updated_at"`
-	SubmissionCount int32     `json:"submission_count"`
-	CommentCount    int32     `json:"comment_count"`
-	CreatedAt       int32     `json:"created_at"`
-}
 
 
 
