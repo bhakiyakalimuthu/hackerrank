f3eb42e02a3e9964f269800b9353ab8a2fb82779
Bhakiyaraj Kalimuthu <bhakiya.kalimuthu@gmail.com>
2020-03-21 07:12:38 +0100

    Add service

---
+++ service.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func client(pageNumber int) (*PageInfo, error) {
+	hostName := "https://jsonmock.hackerrank.com/api/article_users/search?page=" + strconv.Itoa(pageNumber)
+	req, err := http.NewRequest(http.MethodGet, hostName, nil)
+	if err != nil {
+		return nil, err
+	}
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	var page PageInfo
+	err = json.NewDecoder(resp.Body).Decode(&page)
+	if err != nil {
+		return nil, err
+	}
+	return &page, nil
+}
